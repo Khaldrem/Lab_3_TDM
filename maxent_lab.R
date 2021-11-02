91da8d01442fca856439aacc74b164fbdaba06da
Hector Perez <hector.perez.m@hotmail.com>
2021-11-02 13:32:39 -0300

    maxent

---
+++ maxent_lab.R
@@ -0,0 +1,108 @@
+library(maxent)
+library(SnowballC)
+library(tm)
+library(RTextTools)
+library(tidyverse)
+library(ggplot2)
+library(wordcloud)
+
+#Functions
+preprocess_data <- function(data) {
+  #lower corpus
+  data <- tm_map(data, content_transformer(tolower))
+  
+  #remove punctuation
+  data <- tm_map(data, content_transformer(removePunctuation))
+  
+  #remove stop words
+  data <- tm_map(data, content_transformer(removeWords), stopwords("english"))
+  
+  #stemDoc
+  data <- tm_map(data, stemDocument)
+  
+  #remove numbers
+  data <- tm_map(data, content_transformer(removeNumbers))
+  
+  #strip white spaces
+  data <- tm_map(data, stripWhitespace)
+  
+  return(data)
+}
+
+set.seed(999)
+
+#Prepare data
+data("USCongress", package = "RTextTools")
+
+#congress <- USCongress[!(USCongress$major == 99),]
+congress <- USCongress[,]
+
+# topic labels
+major_topics <- tibble(
+  major = c(1:10, 12:21, 99),
+  label = c(
+    "Macroeconomics", "Civil rights, minority issues, civil liberties",
+    "Health", "Agriculture", "Labor and employment", "Education", "Environment",
+    "Energy", "Immigration", "Transportation", "Law, crime, family issues",
+    "Social welfare", "Community development and housing issues",
+    "Banking, finance, and domestic commerce", "Defense",
+    "Space, technology, and communications", "Foreign trade",
+    "International affairs and foreign aid", "Government operations",
+    "Public lands and water management", "Other, miscellaneous"
+  )
+)
+
+(congress <- as_tibble(USCongress) %>%
+    mutate(text = as.character(text)) %>%
+    left_join(major_topics))
+
+
+#Change major 99 to 22 to plot
+congress$major[congress$major == 99] <- 22
+
+#Plot labels
+ggplot(congress, aes(x=major)) + geom_bar(aes(fill=label))
+
+
+
+
+#Separate in train/test
+sample <- sample.int(n = nrow(congress), size = floor(.70 * nrow(congress)), replace = F)
+train <- congress[sample, ]
+test <- congress[-sample, ]
+
+train_corpus <- Corpus(VectorSource(train$text))
+test_corpus <- Corpus(VectorSource(test$text))
+
+#Preprocess both corpus
+train_corpus <- preprocess_data(train_corpus)
+test_corpus <- preprocess_data(test_corpus)
+
+sparse_train <- as.compressed.matrix(DocumentTermMatrix(train_corpus))
+sparse_test <- as.compressed.matrix(DocumentTermMatrix(test_corpus))
+
+#TODO: decidir que nfold usamos
+f <- tune.maxent(sparse_train, train$major, nfold = 3, showall = TRUE, verbose = TRUE)
+print(f)
+
+#Creamos el modelo en base al mejor pct_best_fit anterior
+model <- maxent(sparse_train, congress$major, l1_regularizer = 0.0, l2_regularizer = 0.2, 
+                use_sgd = 0, set_heldout = 0)
+
+#utilizando el dataset test
+results <- predict(model, sparse_test)
+print(results)
+
+
+#Probar con otro nfold
+
+
+#Word cloud with all data
+all_corpus <- Corpus(VectorSource(congress$text))
+all_corpus <- preprocess_data(all_corpus)
+matrix_all_data <- as.matrix(TermDocumentMatrix(all_corpus))
+words <- sort(rowSums(matrix_all_data), decreasing = TRUE)
+df_all_data <- data.frame(word = names(words), freq = words)
+
+wordcloud(words=df_all_data$word,freq=df_all_data$freq, min.freq=1, max.words=300, 
+          random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
